ba3a5b9e4516bc8568983a8d9b293af46ed8261c
Douglas Lowder <dlowder@salesforce.com>
2021-07-17 19:17:33 -0700

    Step 3 (detect button select)

--- App.js
+++ App.js
@@ -13,9 +13,9 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const StatusBar = Platform.isTV ? View : ReactNative.StatusBar;
 
-const Button = ({title, style}) => {
+const Button = ({title, style, onPress}) => {
   return (
-    <TouchableOpacity onPress={() => {}}>
+    <TouchableOpacity onPress={onPress}>
       <View style={style}>
         <Text style={style}>{title}</Text>
       </View>
@@ -25,9 +25,13 @@ const Button = ({title, style}) => {
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const fgColor = isDarkMode ? Colors.lighter : Colors.darker;
+  const bgColor = isDarkMode ? Colors.darker : Colors.lighter;
+
+  const [message, setMessage] = React.useState('');
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: bgColor,
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
@@ -37,15 +41,36 @@ const App = () => {
     fontSize: 24 / PixelRatio.get(),
     padding: 20 / PixelRatio.get(),
     margin: 20 / PixelRatio.get(),
-    color: isDarkMode ? 'white' : 'black',
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    color: fgColor,
+    backgroundColor: bgColor,
+  };
+
+  const messageStyle = {
+    fontSize: 24 / PixelRatio.get(),
+    padding: 20 / PixelRatio.get(),
+    margin: 20 / PixelRatio.get(),
+    color: bgColor,
+    backgroundColor: fgColor,
+    flex: 1,
+    width: '100%',
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Button title="Button 1" style={buttonStyle} />
-      <Button title="Button 2" style={buttonStyle} />
+      <Button
+        title="Button 1"
+        style={buttonStyle}
+        onPress={() => setMessage('Button 1 pressed')}
+      />
+      <Button
+        title="Button 2"
+        style={buttonStyle}
+        onPress={() => setMessage('Button 2 pressed')}
+      />
+      <View style={messageStyle}>
+        <Text style={messageStyle}>{message}</Text>
+      </View>
     </SafeAreaView>
   );
 };
